3d1f397b3cc8abf8801f9dfc848d341c8cc9c932
gokul <gokulyc@gmail.com>
2018-06-15 10:08:17 +0530

    fifth video dump

--- test123/test11.py
+++ test123/test11.py
@@ -1,8 +1,45 @@
 print("Hi1")
-a=10
+a = 10
 print(10)
 
-a=[1,2,3,4,5]
+a = [1, 2, 3, 4, 5]
 a.sort(reverse=True)
 for i in a:
     print(i)
+
+import math
+
+print(dir(math))
+
+from string import whitespace
+
+print("dfbhjvsfhs" + whitespace + str(math.sqrt(1024)))
+
+import sys
+print(sys.version)
+print(sys.argv)
+
+import os
+
+print(os.getcwd())
+print(os.chdir("G://Desktopchai"))
+
+print(os.getcwd())
+'''os.mkdir("G://Desktopchai//edu")
+#os.remove(path of file) removes the file in pah
+
+a=input("enter to continue")
+
+os.rmdir("G://Desktopchai//edu")
+'''
+import random
+print(random.randrange(50.0))
+
+print(random.randrange(0,50.0,10))
+
+print(random.randint(0,50))
+
+import datetime
+print(datetime.datetime.today())
+
+
