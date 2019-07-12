e8eebdcb95ee484a9dfd36fc6b07d0cc0353b13d
Gokul Y <gokulyc@gmail.com>
2019-07-12 14:59:05 +0530

    modified:   test123/Sqlservertest.py

--- test123/Sqlservertest.py
+++ test123/Sqlservertest.py
@@ -7,12 +7,27 @@ import sqlalchemy as sql
  #                       "Database=db_name;"
   #                      "uid=User;pwd=password")
 
-cnxn = pyodbc.connect('driver={SQL Server};'
-                      'server=L-156171445\SQLEXPRESS;'
-                      'database=CRUDDB;'
-                      'trusted_consnection=yes')
+cnxn = pyodbc.connect('driver={SQL Server};''server=L-156171445\SQLEXPRESS;''database=CRUDDB;''trusted_consnection=yes')
 cursor = cnxn.cursor()
-cursor.execute("Select * from Employee")                     
+cursor.execute("""Select count(*) from Employee
+
+""") 
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+cursor.execute("""Insert into Employee Values('Navya11', 'ProjectEngineer1', 241, 500001)
+
+""") 
+# rows = cursor.fetchall()
+# for row in rows:
+#     print(row)
+
+
+cursor.execute("""Select count(*) from Employee
+
+""")   
+                  
 #df = pd.read_sql_query('select * from table', cnxn)
 
 rows = cursor.fetchall()
